41222197eb3e2ca5e6b528c56254a72aa3abdd2d
scott.christie <s.christie496@gmail.com>
2026-06-19 15:43:20 +0100

    test updates post merge

--- src/functionalTest/java/uk/gov/hmcts/reform/probatepostdeploymentfttests/ScenarioSources.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/probatepostdeploymentfttests/ScenarioSources.java
@@ -64,9 +64,8 @@ public class ScenarioSources {
             scenarioPattern = "*" + scenarioPattern + "*.json";
         }
 
-        //scenarioPattern = "probate.json";
-        //scenarioPattern = "probate-task-actions.json";
-        scenarioPattern = "caseprinted-solicitorsmanual-create.json";
+//        scenarioPattern = "caseprinted-solicitorsmanual-create.json";
+//        scenarioPattern = "caseprinted-handleevidence-claim-complete.json";
 
 
         Collection<String> scenarioSources;
