54e4c6826a3fef7a04c594c6e33e7e07f191f695
Mike Brown <michael.brown@hmcts.net>
2026-06-26 12:09:09 +0100

    commented code removed

--- src/functionalTest/java/uk/gov/hmcts/reform/probatepostdeploymentfttests/ScenarioSources.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/probatepostdeploymentfttests/ScenarioSources.java
@@ -64,9 +64,6 @@ public class ScenarioSources {
             scenarioPattern = "*" + scenarioPattern + "*.json";
         }
 
-        scenarioPattern = "caseprinted-amendcasedetails-create.json";
-        //scenarioPattern = "caseprinted-handleevidence-claim-complete.json";
-
         Collection<String> scenarioSources;
         try {
             scenarioSources = StringResourceLoader
